28fde034d935f14309e68ee8d8af981e0e901f45
Gabor Szabo <gabor@szabgab.com>
2025-05-30 10:32:27 +0300

    init

---
+++ check.py
@@ -0,0 +1,43 @@
+import os
+
+checks = {
+    "MDBOOK_VERSION": "0.4.44",
+    "EMBEDIFY_VERSION": "0.2.12",
+    "ALERTS_VERSION": "0.7.0",
+}
+
+def check_repo(base_dir, repo_url):
+    ci = os.environ.get("CI")
+    print(f"CI: {ci}")
+                        
+    
+    print(f"repo_url: {repo_url}")
+    repo_url = repo_url.rstrip("/")
+    repo_name = repo_url.split("/")[-1]
+    print(f"repo_name: {repo_name}")
+    workflows_dir = os.path.join(base_dir, repo_name, ".github", "workflows")
+    print(f"workflows_dir: {workflows_dir}")
+    for file_name in os.listdir(workflows_dir):
+        file_path = os.path.join(workflows_dir, file_name)
+        with open(file_path) as fh:
+            for row in fh:
+                for field, expected_value in checks.items():
+                    if f" {field}:" in row:
+                        row = row.rstrip()
+                        value = row.split(":")[-1].strip()
+                        if value != expected_value:
+                            print(f"Mismatch in {file_name}: field {field}\nexpected value: {expected_value}\n  actual value: {value}")
+                            
+                        #print(f"field {field} expected value: {expected_value} actual value: {value}")
+
+
+def main():
+    base_dir = os.path.dirname(os.getcwd())
+    print(f"base_dir: {base_dir}")
+    for repo_url in [
+        "https://github.com/szabgab/rust.code-maven.com/"
+    ]:
+        check_repo(base_dir, repo_url)
+
+
+main()
